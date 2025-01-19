c7e0c9cbef32f4397ff13d5e87c2b0bd24e5684b
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-19 03:17:50 +0000

    Jan 19, 2025, 3:17 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -12,8 +12,20 @@
 #if not, put in paper
 
 
-Red = int(input("How many red plates are there?")*1)
-Blue = int(input("How many blue plates are there?")*2)
-Green = int(input("How many green plates are there?")*3)
-Ans = Red + Blue + Green
-print (Ans)
+# Red = int(input("How many red plates are there?")*1)
+# Blue = int(input("How many blue plates are there?")*2)
+# Green = int(input("How many green plates are there?")*3)
+# Ans = Red + Blue + Green
+# print (Ans)
+
+score_one = 80
+score_two = 90
+score_three = 75
+
+total = score_one + score_two + score_three
+
+average_score = str(total / 3)
+
+student_name = "Alex"
+
+print("Average score for " + student_name + " is: " + average_score)
