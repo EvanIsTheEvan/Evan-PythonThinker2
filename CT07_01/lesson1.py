f4d085ae0ec9e183fd0903cc4f71b170bc803129
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-19 03:08:48 +0000

    Jan 19, 2025, 3:08 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,19 @@
-print("Hello from lesson 1")
 
+
+#make glass box
+#make paper box
+#make plastic box
+
+#repeat until 0 items
+#if item is glass then
+    #put in glass
+#if not, check if item is plastic
+    #put in plastic
+#if not, put in paper
+
+
+Red = int(input("How many red plates are there?")*1)
+Blue = int(input("How many blue plates are there?")*2)
+Green = int(input("How many green plates are there?")*3)
+Ans = Red + Blue + Green
+print (Ans)
--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,6 @@
-print("Hello from lesson 2")
+# print("Hello from lesson 2")
+# # print("Hi")
+# num1 = input("Give me a number")
+# print(num1)
 
+#
