3d0706f75178f01fba7dd7a6183c0c5f2553f68d
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 03:00:20 +0000

    Jan 26, 2025, 3:00 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -18,14 +18,61 @@
 # Ans = Red + Blue + Green
 # print (Ans)
 
-score_one = 80
-score_two = 90
-score_three = 75
+# score_one = 80
+# score_two = 90
+# score_three = 75
 
-total = score_one + score_two + score_three
+# total = score_one + score_two + score_three
 
-average_score = str(total / 3)
+# average_score = str(total / 3)
 
-student_name = "Alex"
+# student_name = "Alex"
 
-print("Average score for " + student_name + " is: " + average_score)
+# print("Average score for " + student_name + " is: " + average_score)
+
+# score = int(input("What did you get for the test? Out of 100"))
+# if score > 74:
+#     grade = "A"
+# elif score>59:
+#     grade= "B"
+# elif score>49:
+#     grade= "C"
+# else:
+#     grade="FAIL"
+
+# print (grade)
+
+# x = range(0, 21)
+# for n in x:
+#   print(n)
+
+# e = range(1, 31)
+# for a in e:
+#   print(a)
+
+# b = range(2, 25 , 2)
+# for c in b:
+#   print(c)
+
+# counter = 0
+# while counter <= 20:
+#     print(counter)
+#     counter += 1
+
+# counter1 = 1
+# while counter <= 30:
+#     print(counter1)
+#     counter1 += 1
+
+# counter2 = 2
+# while counter2 <= 24:
+#     print(counter2)
+#     counter2 += 1
+
+counter = 0
+while counter <= 10:
+    if counter == 6:
+        (exit)
+    else:
+        print(counter)
+        counter += 1
