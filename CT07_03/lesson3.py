7791965b7a22f946358e902c6e88aa4a40149c72
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 04:18:49 +0000

    Jan 26, 2025, 4:18 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -8,6 +8,6 @@ while counter != 0:
     counter=counter-1
     time.sleep(1)
 str(HOWLONGUWANT)
-print("You studied for "+str(HOWLONGUWANT)+" seconds or"+str(Hi)+" minutes and gained 0 brain cells.")
+print("You studied for "+str(HOWLONGUWANT)+" seconds or "+str(Hi)+" minutes and gained 0 brain cells.")
 
 
