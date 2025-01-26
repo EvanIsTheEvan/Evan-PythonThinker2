dd74ee8b9351d7b2224c288ef7af3517d7656ffe
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 04:21:04 +0000

    Jan 26, 2025, 4:21 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -2,12 +2,13 @@ import time
 
 HOWLONGUWANT = int(input("How many seconds do you want to study for?"))
 Hi=HOWLONGUWANT/60
+
 counter= HOWLONGUWANT
 while counter != 0:
     print(counter)
     counter=counter-1
     time.sleep(1)
 str(HOWLONGUWANT)
-print("You studied for "+str(HOWLONGUWANT)+" seconds or "+str(Hi)+" minutes or "+ HII +"hours and gained 0 brain cells.")
+print("You studied for "+str(HOWLONGUWANT)+" seconds or "+str(Hi)+" minutes or "+ str(HII) +"hours and gained 0 brain cells.")
 
 
