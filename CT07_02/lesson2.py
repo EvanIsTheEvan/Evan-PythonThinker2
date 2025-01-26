4c534a14c5d3be27c54b92484af8eb96176c4747
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 03:24:18 +0000

    Jan 26, 2025, 3:24 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -7,9 +7,9 @@ question_answer = ["IDK","2","4"]
 
 
 answer="erfghjjjjjjjjjjjjjjjjmdsssss"
-while answer != question_answer :
+while answer != question_answer[0] :
     answer = input("What is the mass of the sun?")
-while answer != question_answer :
+while answer != question_answer[1] :
     answer = input("What is 1+1")
 while answer != question_answer :
     answer = input("2+2")    
