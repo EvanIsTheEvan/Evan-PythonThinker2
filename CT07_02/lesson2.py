241ad51c5a2147e06f18d645f7173dd388fac3b8
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 03:25:10 +0000

    Jan 26, 2025, 3:25 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -9,8 +9,9 @@ question_answer = ["IDK","2","4"]
 answer="erfghjjjjjjjjjjjjjjjjmdsssss"
 while answer != question_answer[0] :
     answer = input("What is the mass of the sun?")
-else:print
+else:print("correct")
 while answer != question_answer[1] :
     answer = input("What is 1+1")
+    el
 while answer != question_answer[2] :
     answer = input("2+2")    
