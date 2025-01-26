008439e631eaf36444a83e07a1a1b663b9df1791
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 04:21:51 +0000

    Jan 26, 2025, 4:21 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -7,7 +7,7 @@ counter= HOWLONGUWANT
 while counter != 0:
     print(counter)
     counter=counter-1
-    time.sleep(1)
+    time.sleep(60)
 str(HOWLONGUWANT)
 print("You studied for "+str(HOWLONGUWANT)+" seconds or "+str(Hi)+" minutes or "+ str(HII) +"hours and gained 0 brain cells.")
 
