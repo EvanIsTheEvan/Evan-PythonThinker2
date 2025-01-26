479fa55016ac258c0f67ca072b5199f1c3ef1ad6
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 04:33:04 +0000

    Jan 26, 2025, 4:33 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -3,6 +3,7 @@ import time
 HOWLONGUWANT = int(input("How many seconds do you want to study for?"))
 Hi=HOWLONGUWANT/60
 HII=HOWLONGUWANT/3600
+HIII=HOWLONGUWANT/86400
 counter= HOWLONGUWANT
 while counter != 0:
     print(counter)
