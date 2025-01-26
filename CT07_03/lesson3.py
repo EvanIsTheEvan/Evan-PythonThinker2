9c5a88fe4ab8cf11d1fe1217cfb9ef8257585341
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 04:12:30 +0000

    Jan 26, 2025, 4:12 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -2,6 +2,7 @@ import time
 HOWLONGUWANT = int(input("How long do you want to study for?"))
 counter= HOWLONGUWANT
 while counter != 0:
+    print(counter)
     counter=counter-1
     time.sleep(1)
 
