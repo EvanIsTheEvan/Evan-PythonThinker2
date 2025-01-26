7c9c950684d3722539347a92a14244c478d4c39f
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-01-26 03:59:57 +0000

    Jan 26, 2025, 3:59 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -12,6 +12,7 @@ attempts = 0
 while True:
     while answr != question_answer[0] :
         answr = input("What is the mass of the sun?")
+        attempt+=1
     print("CORRECT")
     while answr != question_answer[1] :
         answr = input("What is 1+1")
