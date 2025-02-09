a76b24eb4017a92d07afbc705a2004f18d5fd0fd
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-09 04:27:24 +0000

    Feb 9, 2025, 4:27 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -75,4 +75,4 @@ while not ans == "end":
     i=i+1
 for a in range(len(MEENU)-1):
     b=a+1
-    print(MEENU[a] +" is now in the menu as the " + str(b) +"th item.")
+    print(MEENU[a] +" is now in the menu as the " + str(b) +" item.")
