aaaa40cebb4333bcd68ea53b35e09ac0a9e7f9d1
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-09 03:00:45 +0000

    Feb 9, 2025, 3:00 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -3,29 +3,30 @@
 # num1 = input("Give me a number")
 # print(num1)
 
-question_answer = ["IDK","2","4"]
+# question_answer = ["IDK","2","4"]
 
 
 
-answr="erfghjjjjjjjjjjjjjjjjmdsssss"
-attempts = 0
-while True:
-    while answr != question_answer[0] :
-        answr = input("What is the mass of the sun?")
-        attempts+=1
-        if attempts>3:
-            break
-    print("CORRECT")
-    while answr != question_answer[1] :
-        answr = input("What is 1+1")
-        attempts+=1
-        if attempts>3:
-            break
-    print("CORRECT")
-    while answr != question_answer[2] :
-        answr = input("What is 2+2")
-        attempts+=1
-        if attempts>3:
-            break
-    print("CORRECT")
-    break
+# answr="erfghjjjjjjjjjjjjjjjjmdsssss"
+# attempts = 0
+# while True:
+#     while answr != question_answer[0] :
+#         answr = input("What is the mass of the sun?")
+#         attempts+=1
+#         if attempts>3:
+#             break
+#     print("CORRECT")
+#     while answr != question_answer[1] :
+#         answr = input("What is 1+1")
+#         attempts+=1
+#         if attempts>3:
+#             break
+#     print("CORRECT")
+#     while answr != question_answer[2] :
+#         answr = input("What is 2+2")
+#         attempts+=1
+#         if attempts>3:
+#             break
+#     print("CORRECT")
+#     break
+
