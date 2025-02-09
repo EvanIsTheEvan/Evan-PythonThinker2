1e822edcf9c33cab3b9db883b5dff3462350445b
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-09 03:31:06 +0000

    Feb 9, 2025, 3:31 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -25,8 +25,8 @@ num1=0
 ans=0
 Ans=1
 for i in range(1,15):
-    num1=int(random.randint(2,20))
-    num2=random.randint(2,20)
+    num1=int(random.randint(2,3))
+    num2=random.randint(2,3)
     Ans=num1*num2
     while not ans==Ans:
         ans=int(input("What is "+str(num1)+" x "+str(num2)+" ?"))
