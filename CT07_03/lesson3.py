358a5ca66775dcbced416cd26a98a20aabd91858
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-09 04:28:23 +0000

    Feb 9, 2025, 4:28 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -76,5 +76,5 @@ while not ans == "end":
 for a in range(len(MEENU)-1):
     b=a+1
     if b==1:
-        b=str(b+"st"
+        b=str(b)+"st"
     print(MEENU[a] +" is now in the menu as the " + str(b) +" item.")
