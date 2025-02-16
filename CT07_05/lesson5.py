fa86be9be36d091b7385d4b6c56643f4fc254b37
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-16 03:42:43 +0000

    Feb 16, 2025, 3:42 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,10 +1,35 @@
 # print("Hello from lesson 5")
 
 
+# sadg=list()
+# import random
+# for i in range(100):
+#     sgnuiehguaewgh87enyergew=random.randint(1,1000)
+#     sadg.append(sgnuiehguaewgh87enyergew)
+# print (sadg)
+
+# num=69
+# if num in sgnuiehguaewgh87enyergew:
+#     print("YEES IT IN")
+# else:
+#     ("NOOT IN")
+
 sadg=list()
 import random
 for i in range(100):
     sgnuiehguaewgh87enyergew=random.randint(1,1000)
+    if sadg=list()
+import random
+for i in range(100):
+    sgnuiehguaewgh87enyergew=random.randint(1,1000)
+    sadg.append(sgnuiehguaewgh87enyergew)
+print (sadg)
+
+num=69
+if num in sgnuiehguaewgh87enyergew:
+    print("YEES IT IN")
+else:
+    ("NOOT IN")
     sadg.append(sgnuiehguaewgh87enyergew)
 print (sadg)
 
