89301836a9e13e6ca862b62d9e89419d8c06a1bc
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-16 03:40:03 +0000

    Feb 16, 2025, 3:40 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -7,3 +7,7 @@ for i in range(100):
     sgnuiehguaewgh87enyergew=random.randint(1,1000)
     sadg.append(sgnuiehguaewgh87enyergew)
 print (sadg)
+
+num=69
+if num in sgnuiehguaewgh87enyergew:
+    print("")
