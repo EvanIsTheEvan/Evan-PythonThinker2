0d34c918bff48f49fa21583342ca92387a76d864
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-16 03:33:33 +0000

    Feb 16, 2025, 3:33 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -5,4 +5,4 @@ sadg=list()
 import random
 for i in (100):
     sgnuiehguaewgh87enyergew=random.randint(1,1000)
-    list.appends[str(sgnuiehguaewgh87enyergew)] 
+    sadg.appends[str(sgnuiehguaewgh87enyergew)] 
