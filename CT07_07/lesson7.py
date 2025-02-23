f3605e3a85c3cec5ec73bc7a612cde0cd42a02b3
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-23 04:12:16 +0000

    Feb 23, 2025, 4:12 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -3,5 +3,5 @@ student1=["John","67894903","Hockey"]
 student2=["Tom","25348033","Badminton"]
 student3=["Timothy","36728193","Soccer"]
 students.append(student3)
-students.append(student3)
-students.append(student3)
+students.append(student2)
+students.append(student1)
