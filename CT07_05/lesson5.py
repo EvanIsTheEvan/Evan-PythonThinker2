859c51c4c3f05801dea897ae2e899bc500347abd
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-23 03:00:13 +0000

    Feb 23, 2025, 3:00 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -14,27 +14,30 @@
 # else:
 #     ("NOOT IN")
 
-sadg=list()
-import random
-for i in range(100):
-    sgnuiehguaewgh87enyergew=random.randint(1,1000)
-    if sadg=list()
-import random
-for i in range(100):
-    sgnuiehguaewgh87enyergew=random.randint(1,1000)
-    sadg.append(sgnuiehguaewgh87enyergew)
-print (sadg)
+# sadg=list()
+# import random
+# for i in range(100):
+#     sgnuiehguaewgh87enyergew=random.randint(1,1000)
+#     while sgnuiehguaewgh87enyergew in sadg:
+#         sgnuiehguaewgh87enyergew=random.randint(1,1000)
+#     sadg.append(sgnuiehguaewgh87enyergew)
+# print (sadg)
 
-num=69
-if num in sgnuiehguaewgh87enyergew:
-    print("YEES IT IN")
-else:
-    ("NOOT IN")
-    sadg.append(sgnuiehguaewgh87enyergew)
-print (sadg)
+# num=69
+# if num in sgnuiehguaewgh87enyergew:
+#     print("YEES IT IN")
+# else:
+#     ("NOOT IN")
 
-num=69
-if num in sgnuiehguaewgh87enyergew:
-    print("YEES IT IN")
-else:
-    ("NOOT IN")
+namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+            "Sophia", "Lucas", "Mia", "Aiden"
+            ]
+heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+
+dslaf=max(heightlist)
+ajfids=heightlist.index(dslaf)
+print ((namelist[ajfids])+"is the tallest at "+str(dslaf)+" cm tall.")
+
+aoisd=min(heightlist)
+bsd=heightlist.index(aoisd)
+print ((namelist[bsd])+"is the shortest at "+str(aoisd)+" cm tall.")
