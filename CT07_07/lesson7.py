268c19baa86d9586fcdf64bb46e9c1b86283cfe6
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-23 04:21:51 +0000

    Feb 23, 2025, 4:21 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,14 +1,16 @@
-students=[]
-student1=["John","67894903","Hockey"]
-student2=["Tom","25348033","Badminton"]
-student3=["Timothy","36728193","Soccer"]
-students.append(student3)
-students.append(student2)
-students.append(student1)
+# students=[]
+# student1=["John","67894903","Hockey"]
+# student2=["Tom","25348033","Badminton"]
+# student3=["Timothy","36728193","Soccer"]
+# students.append(student3)
+# students.append(student2)
+# students.append(student1)
 
 
-for student in students:
-    name,phonenumber,CCA = student
-    print("Name:"+ str(name))
-    print("Phone Number:"+ str(phonenumber))
-    print("CCA:"+ str(CCA))
+# for student in students:
+#     name,phonenumber,CCA = student
+#     print("Name:"+ str(name))
+#     print("Phone Number:"+ str(phonenumber))
+#     print("CCA:"+ str(CCA))
+
+
