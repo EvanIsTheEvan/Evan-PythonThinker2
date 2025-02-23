0a4bdd5a546f27f7a44e130ef4844343f024ec13
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-23 04:10:41 +0000

    Feb 23, 2025, 4:10 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,4 +1,4 @@
 students=[]
 student1=["John","67894903","Hockey"]
-student1=["John","67894903","Hockey"]
-student1=["John","67894903","Hockey"]
+student2=["Tom","67894903","Hockey"]
+student3=["John","67894903","Hockey"]
