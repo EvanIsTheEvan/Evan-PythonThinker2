53b2096394a3011ed484b6d64f129d147e015c93
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-02-23 03:42:21 +0000

    Feb 23, 2025, 3:42 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,12 +1,18 @@
 
-contacts = []
-contact1 = ["John", 98453126, "john@gmail.com"]
-contact2 = ["Adam", 93029102, "adam@gmail.com"]
-contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
-contacts.append(contact1)
-contacts.append(contact2)
-contacts.append(contact3)
+# contacts = []
+# contact1 = ["John", 98453126, "john@gmail.com"]
+# contact2 = ["Adam", 93029102, "adam@gmail.com"]
+# contact3 = ["Sylvia", 87894032, "sylvia@gmail.com"]
+# contacts.append(contact1)
+# contacts.append(contact2)
+# contacts.append(contact3)
 
-for fusnunsinadsjknfsdafiuashdijnadsfhausifusdnfadsjfhiueasuidnfjfhiaeufh in contacts:
-    for jjdsgiuifuewughuahsiudfhdusigh in fusnunsinadsjknfsdafiuashdijnadsfhausifusdnfadsjfhiueasuidnfjfhiaeufh:
-        print(jjdsgiuifuewughuahsiudfhdusigh)
+# for fusnunsinadsjknfsdafiuashdijnadsfhausifusdnfadsjfhiueasuidnfjfhiaeufh in contacts:
+#     for jjdsgiuifuewughuahsiudfhdusigh in fusnunsinadsjknfsdafiuashdijnadsfhausifusdnfadsjfhiueasuidnfjfhiaeufh:
+#         print(jjdsgiuifuewughuahsiudfhdusigh)
+
+["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
