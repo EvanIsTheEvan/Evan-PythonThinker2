63b72f3df068bb9e0c3f0977d8ec9289f2fae4fb
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:29:10 +0000

    Mar 9, 2025, 3:29 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -1,2 +1,2 @@
 print("Hero starts on his adventure with Health: 100")
-jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=
+jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=100
