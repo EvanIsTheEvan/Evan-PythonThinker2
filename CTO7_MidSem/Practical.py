45de514c67c4e272eaa094d5452369599d99dafd
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:32:38 +0000

    Mar 9, 2025, 3:32 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -4,4 +4,4 @@ jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=100
 fnasvudbuvadbsuvbjadsvbajybvyuhsd=random.randint(1,15)
 while jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8>0:
     jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8-fnasvudbuvadbsuvbjadsvbajybvyuhsd
-    print("After fighting monsters, his Health is now:"+"" )
+    print("After fighting monsters, his Health is now: "+jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8 )
