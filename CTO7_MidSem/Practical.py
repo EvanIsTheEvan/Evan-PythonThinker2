1feb1544c97c49043224b26467459e89614147e1
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:42:20 +0000

    Mar 9, 2025, 3:42 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -1,12 +1,13 @@
-import random
-print("Hero starts on his adventure with Health: 100")
-health=100
-randomhealth=random.randint(1,15)
-battles=0
-while health>0:
-    health=health-randomhealth
-    battles=battles+1
-    if health<0 or health==0:
-        break
-    print("After fighting monsters, his Health is now: "+str(health) )
-print("He fought "+str(battles-1)+" battles, and died.")
+# import random
+# print("Hero starts on his adventure with Health: 100")
+# health=100
+# randomhealth=random.randint(1,15)
+# battles=0
+# while health>0:
+#     health=health-randomhealth
+#     battles=battles+1
+#     if health<0 or health==0:
+#         break
+#     print("After fighting monsters, his Health is now: "+str(health) )
+# print("He fought "+str(battles-1)+" battles, and died.")
+
