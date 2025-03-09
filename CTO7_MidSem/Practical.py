48dc5f02bc0c4b2e32b3dee8884cbcf5ace112b8
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:39:03 +0000

    Mar 9, 2025, 3:39 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -2,9 +2,10 @@ import random
 print("Hero starts on his adventure with Health: 100")
 health=100
 randomhealth=random.randint(1,15)
+battles=0
 while health>0:
     health=health-randomhealth
     if health<0 or health==0:
         break
     print("After fighting monsters, his Health is now: "+str(health) )
-print("After fighting monsters, his Health is now:"+str(health)+". He fought "+battles+"")
+print("After fighting monsters, his Health is now:"+str(health)+". He fought "+battles+", and died.")
