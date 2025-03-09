4a99ee09de68f524dc17c502028ecad85ed91aca
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 04:06:30 +0000

    Mar 9, 2025, 4:06 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -1,25 +1,25 @@
-# import random
-# print("Hero starts on his adventure with Health: 100")
-# health=100
-# randomhealth=random.randint(1,15)
-# battles=0
-# while health>0:
-#     health=health-randomhealth
-#     battles=battles+1
-#     if health<0 or health==0:
-#         break
-#     print("After fighting monsters, his Health is now: "+str(health) )
-# print("He fought "+str(battles-1)+" battles, and died.")
-
-food=[]
-foodrequest=0
-num=0
-while not foodrequest=="end":
-    foodrequest=input("What would you like to order?")
-    if foodrequest=="end":
+import random
+print("Hero starts on his adventure with Health: 100")
+health=100
+randomhealth=random.randint(1,15)
+battles=0
+while health>0:
+    health=health-randomhealth
+    battles=battles+1
+    if health<0 or health==0:
         break
-    food.append(foodrequest)
-print("You have ordered the following:")
-for i in food:
-    num=num+1
-    print(str(num)+". "+i)
+    print("After fighting monsters, his Health is now: "+str(health) )
+print("He fought "+str(battles-1)+" battles, and died.")
+
+# food=[]
+# foodrequest=0
+# num=0
+# while not foodrequest=="end":
+#     foodrequest=input("What would you like to order?")
+#     if foodrequest=="end":
+#         break
+#     food.append(foodrequest)
+# print("You have ordered the following:")
+# for i in food:
+#     num=num+1
+#     print(str(num)+". "+i)
