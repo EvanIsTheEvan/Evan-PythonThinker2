7a10d2df5b80c1c70e3b7fd033715218a29605f1
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:30:52 +0000

    Mar 9, 2025, 3:30 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -1,6 +1,7 @@
 import random
 print("Hero starts on his adventure with Health: 100")
 jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=100
-fnasvudbuvadbsuvbjadsvbajybvyuhsd=
+fnasvudbuvadbsuvbjadsvbajybvyuhsd=random.randint(1,15)
 while jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8>0:
     jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8-fnasvudbuvadbsuvbjadsvbajybvyuhsd
+    
