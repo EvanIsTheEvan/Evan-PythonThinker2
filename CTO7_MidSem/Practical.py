443190dade103363cf89468c789a0f1a82bf0fff
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:39:30 +0000

    Mar 9, 2025, 3:39 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -5,7 +5,8 @@ randomhealth=random.randint(1,15)
 battles=0
 while health>0:
     health=health-randomhealth
+    battles=battles+1
     if health<0 or health==0:
         break
     print("After fighting monsters, his Health is now: "+str(health) )
-print("After fighting monsters, his Health is now:"+str(health)+". He fought "+battles+", and died.")
+print("After fighting monsters, his Health is now:"+str(health)+". He fought "+str(battles)+", and died.")
