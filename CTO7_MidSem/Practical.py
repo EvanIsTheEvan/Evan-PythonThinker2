67383b59e5bb74cdce011817b97490e0946e9144
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:37:43 +0000

    Mar 9, 2025, 3:37 AM

--- CTO7_MidSem/Practical.py
+++ CTO7_MidSem/Practical.py
@@ -1,10 +1,10 @@
 import random
 print("Hero starts on his adventure with Health: 100")
-jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=100
-fnasvudbuvadbsuvbjadsvbajybvyuhsd=random.randint(1,15)
-while jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8>0:
-    jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8=jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8-fnasvudbuvadbsuvbjadsvbajybvyuhsd
-    if jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8<0 or jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8==0:
+health=100
+randomhealth=random.randint(1,15)
+while health>0:
+    health=health-randomhealth
+    if health<0 or health==0:
         break
-    print("After fighting monsters, his Health is now: "+str(jhvbeyufgeifhmeirhgvy487fqdsiovvyu54hrd8923hmf74yr89ewbfy94hymr8) )
-print("After fighting monsters, his Health is now: 3. He fought XXX battles, and died.")
+    print("After fighting monsters, his Health is now: "+str(health) )
+print("After fighting monsters, his Health is now: ")
