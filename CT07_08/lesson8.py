e9bf6c19f2726de5b17018041de5d6de8d9430ae
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-09 03:19:28 +0000

    Mar 9, 2025, 3:19 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,1 +1,36 @@
-print("Hello from lesson 8")
+# fruits=["Apple","Banana","Cherry","Durian","Elderberry","Figs"]
+# index=len(fruits)//2
+# left=print(fruits[:index])
+# right=print(fruits[index:])
+
+
+# fruits=["Apple","Banana","Cherry","Durian",]
+# fruits2=["Cherry","Durian","Elderberry","Figs"]
+# common=[]
+# for i in fruits:
+#     if i in fruits:
+#         if i in fruits2:
+#             common.append(i)
+
+# print(common)
+
+
+# fruits=["Apple","Banana","Cherry","Durian",]
+# fruits2=["Cherry","Durian","Elderberry","Figs"]
+# fruitz=fruits+fruits2
+# unique=[]
+# for i in fruitz:
+#     if i in fruits and not i in fruits2:
+#         unique.append(i)
+#     if i in fruits2 and not i in fruits:
+#         unique.append(i)        
+# print(unique)
+
+num=[1,2,3,4,]
+num2=[5,6,7,8]
+numz=num+num2
+even=[]
+for i in numz:
+    if i % 2 == 0:
+        even.append(i)
+print(even)
