679dc8c4591c23055d7e910e6ca804c4cf4b4e9f
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-30 04:11:54 +0000

    Mar 30, 2025, 4:11 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -45,6 +45,6 @@ BANNANABOI3456.seth(90)
 BANNANABOI3456.shape("turtle")
 BANNANABOI3456.color("#9A3836")
 BANNANABOI3456.goto(0,-250)
-BANNANABOI3456.write(align="center")
+BANNANABOI3456.write(align="center",font=('Arial'))
 
 window.mainloop()
