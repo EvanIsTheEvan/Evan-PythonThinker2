23cb58a22ccf5874fcf9f97252b19399d232bcc3
EvanIsTheEvan <evanlimey0425@gmail.com>
2025-03-30 03:59:44 +0000

    Mar 30, 2025, 3:59 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -49,25 +49,130 @@
 # print (list420[list220])
 # print (list420[list221])
 
-string="gjiusaniufnsaifnsdjafnaiusdnfiuadsnfijadsnijdsanuicshn"
-hasupper=False
-haslower=False
-hasdigit=False
-hasalnum=False
-if len(string)>7:
-    for char in string:
-        if char.isupper:
-            hasupper=True 
-        if char.islower:
-            haslower=True
-        if char.isdigit:
-            hasdigit=True
-    if string.isalnum:
-            hasalnum=True
-    if hasupper==True:
-        if haslower==True:
-            if hasdigit==True:
-                if hasalnum==True:
-                    print("DA PASSWORD IS VALID!")
-                else:
-                    print("INVALID!")
+# string=(input("What is your selected password?"))
+# hasupper=False
+# haslower=False
+# hasdigit=False
+# hasalnum=False
+# if len(string)>7:
+#     for char in string:
+#         if char.isupper:
+#             hasupper=True 
+#         if char.islower:
+#             haslower=True
+#         if char.isdigit:
+#             hasdigit=True
+#     if string.isalnum:
+#             hasalnum=True
+#     if hasupper==True:
+#         if haslower==True:
+#             if hasdigit==True:
+#                 if hasalnum==True:
+#                     print("DA PASSWORD IS VALID!")
+#                 else:
+#                     print("INVALID!")
+
+
+# text=input("Sentence:")
+# alt = True
+# output=""
+
+# for i in text:
+#     if alt == True:
+#         output += i.upper()
+#         alt=False
+#     else:
+#         output += i.lower()
+#         alt=True
+
+# print(output)
+
+# string = "Computers empower our modern world with their digital brains."
+# string=string.split()
+# print(string)
+
+# string = "Computers,empower,our,modern,world,with,their,digital,brains."
+# string=string.split(",")
+# print(string)
+
+# list=['Computers', 'empower', 'our', 'modern', 'world', 'with', 'their', 'digital', 'brains.']
+# string= " ".join(list)
+# print(string)
+
+# list=['Computers', 'empower', 'our', 'modern', 'world', 'with', 'their', 'digital', 'brains.']
+# string= ",".join(list)
+# print(string)
+
+# string = "Hello World"
+
+# string=string.split()
+# for i in string:
+#     i=i[::-1]
+#     string=" ".join(i)
+# print(string)
+
+# word = "radar"
+# is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd = word == word[::-1]
+# print("Input: "+word[::1])
+# print("Output: "+str(is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd))
+
+# while True:
+#     word = input("Gimme a word.")
+#     if word == "end":
+#         break
+#     is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd = word == word[::-1]
+#     print("Input: "+word[::1])
+#     print("Output: "+str(is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd))
+
+
+# poolendroms=0
+# pillendrems=[]
+# string = input("Gimme a sentence")
+# string=string.split()
+# for i in string:
+#     is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd = i == i[::-1]
+#     if is_pakljdsaiushdfuiabchjrbyfsedjbuh2q498ewufaIjd == True:
+#         poolendroms=poolendroms+1
+#         pillendrems.append(i)
+# print(poolendroms)
+# print(pillendrems)
+
+# sales_data = [
+#     ["Appel",50,1.99],
+#     ["Banana",45,0.99],
+#     ["Orange",30,2.49],
+#     ["Grapefruit",25,3.99],
+#     ["Mango",20,2.99]
+# ]
+
+# sorted_earned=[]
+# earned=[]
+
+# for fruits in sales_data:
+#     fruit, sold, price = fruits
+#     earned.append(sold*price)
+
+# earned.sort()
+# print(earned)
+
+
+
+
+# hacking101=[
+#     ["SUSSYAMONGUS3487289","5NightsAtDiddy's","Lemonsquezzer.com"],
+# ]
+# whatuwantbro=input("What do you want to do with the terminal? Choose Add, Search, Display or Quit.")
+# while not whatuwantbro=="Quit":
+#     if whatuwantbro == "Add":
+#         whichpassword=input("Which website's password do you want to edit")
+#         for i in hacking101:
+#             User,Pass,URL=i
+#             if URL == whichpassword:
+#                 addpasswerd=input("What do you want to add to the password?")
+#                 i.append(addpasswerd)
+#     if whatuwantbro =="Search":
+#         whichsearch=input("Which website's password do you want to search")
+#         for i in hacking101:
+#             User,Pass,URL=i
+#             if URL == whichsearch:
+#                 searchwut=("")
--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,2 +1,43 @@
-print("Hello from lesson 9")
+# IDIOTRESPONSE=input("What has to be broken before you can use it?")
+# isCorrect=False
+# IDIOTRESPONSELOWER=IDIOTRESPONSE.lower()
+# IDIOTRESPONSESPLITTED= IDIOTRESPONSELOWER.split()
+# for i in IDIOTRESPONSESPLITTED:
+#     if i == "egg":
+#         isCorrect=True
+# if isCorrect==True:
+#     print("NOT BAD YOU IDIOT...")
+# else:
+#     print("I KNEW IT YOU FAILED!")
 
+# print(IDIOTRESPONSE)
+
+# import turtle as t
+
+# window=t.Screen()
+# window.setup(width=969, height=696)
+# window.bgcolor("forestgreen")
+# window.mainloop()
+
+import turtle as t
+
+window=t.Screen()
+window.setup(width=696,height=696)
+window.bgcolor("forestgreen")
+
+pen=t.Turtle()
+pen.penup()
+pen.shape("square")
+pen.sety(250)
+for i in range(-696,696,25):
+    pen.setx(i)
+    pen.stamp()
+
+pen.goto(-300,-250)
+pen.color("yellow")
+pen.pendown()
+pen.seth(0)
+pen.forward(696)
+pen.hideturtle()
+
+window.mainloop()
